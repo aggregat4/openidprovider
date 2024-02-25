05f2d9ae38cac641a5f41798abf10c37df3d072e
Boris Terzic <boris@terzic.be>
2024-02-25 13:33:44 +0100

    refactor: Adding two utility methods to load public and private RSA keys

--- pkg/crypto/crypto.go
+++ pkg/crypto/crypto.go
@@ -1,6 +1,12 @@
 package crypto
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rsa"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -10,4 +16,28 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func ReadRSAPrivateKey(filename string) (*rsa.PrivateKey, error) {
+	privateKeyFile, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyFile)
+	if err != nil {
+		return nil, err
+	}
+	return privateKey, err
+}
+
+func ReadRSAPublicKey(filename string) (*rsa.PublicKey, error) {
+	publicKeyFile, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(publicKeyFile)
+	if err != nil {
+		return nil, err
+	}
+	return publicKey, nil
 }
