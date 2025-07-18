df22e74d049b64930b010e2a47533ff9d9a14e0e
Boris Terzic <boris@terzic.be>
2025-07-18 15:30:31 +0200

    Remove unused build and test scripts

--- scripts/build-in-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This will build the program inside a docker container so that it links against an older
-# version of libc and makes it easier to run on somewhat older servers
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.21 scripts/build.sh
